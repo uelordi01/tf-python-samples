e47b9b83f10be848e45ca9b48f8e2ebff77aeb00
Unai <elordiae@gmail.com>
2019-04-08 17:23:53 +0200

    trying to add summary values in the script

--- tensorflow_learning/minst_perceptron.py
+++ tensorflow_learning/minst_perceptron.py
@@ -1,6 +1,20 @@
 import tensorflow as tf
 
 from tensorflow.examples.tutorials.mnist import input_data
+
+
+def variable_summaries(var):
+  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+  with tf.name_scope('summaries'):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean', mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev', stddev)
+    tf.summary.scalar('max', tf.reduce_max(var))
+    tf.summary.scalar('min', tf.reduce_min(var))
+    tf.summary.histogram('histogram', var)
+
 mnist_data = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 
@@ -9,13 +23,17 @@ no_classes = 10
 batch_size = 100
 total_batches = 200
 
+
 # x_input is the input image
 x_input = tf.placeholder(tf.float32, shape=[None, input_size])
 y_input = tf.placeholder(tf.float32, shape=[None, no_classes])
 
 # defining fully connected layers:
 weights = tf.Variable(tf.random_normal([input_size, no_classes]))
+variable_summaries(weights)
+
 bias = tf.Variable(tf.random_normal([no_classes]))
+variable_summaries(bias)
 
 logits = tf.matmul(x_input, weights) + bias
 
@@ -27,6 +45,7 @@ softmax_cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
 labels=y_input, logits=logits)
 # we compute the loss function
 loss_operation = tf.reduce_mean(softmax_cross_entropy)
+variable_summaries(loss_operation)
 # we apply the backpropagation through gradient descent to the loss function
 
 optimiser = tf.train.GradientDescentOptimizer(
@@ -37,16 +56,23 @@ learning_rate=0.5).minimize(loss_operation)
 # updated. The variables have not yet been initialized. Next, start the session and initialize the
 # variables using a global variable initializer:
 
-session = tf.Session()
-session.run(tf.global_variables_initializer())
 
+summ = tf.summary.merge_all()
+
+
+session = tf.Session()
+train_writer = tf.summary.FileWriter( 'train',
+                                     session.graph)
+session.run(tf.global_variables_initializer())
+counter = 0;
 for batch_no in range(total_batches):
     mnist_batch = mnist_data.train.next_batch(batch_size)
-    _, loss_value = session.run([optimiser, loss_operation], feed_dict={
+    summary, loss_value  = session.run([optimiser, loss_operation], feed_dict={
     x_input: mnist_batch[0],
     y_input: mnist_batch[1]
     })
     print(loss_value)
+    train_writer.add_summary(summary, counter)
 # for batch_no in range(total_batches)
 #     mnist_batch = mnist_data.train.next_batch(batch_size),
 #
@@ -59,5 +85,6 @@ accuracy_value = session.run(accuracy_operation, feed_dict={
 x_input: test_images,
 y_input: test_labels
 })
+
 print('Accuracy : ', accuracy_value)
 session.close()
